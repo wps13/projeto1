e6af4b62be0457e13ce3a6b3fee2cb575bb1779b
wps13 <willanepaiva@hotmail.com>
2017-09-08 13:02:26 -0300

    Pequenas alterações

--- ponto.cpp
+++ ponto.cpp
@@ -8,28 +8,29 @@ ponto::ponto()
 
 void setX(float _x) //define a coordenada x do ponto
 {
-
+x=_x;
 
 }
 
 void setY(float _y)//define a coordenada y do ponto
 {
-
+y=_y;
 }
 
 void setXY(float _x, float _y)  //define x e y do ponto
 {
-
+x=_x;
+y=_y;
 }
 
 float getX() //retorna a coordenada x do ponto
 {
-
+return x;
 }
 
 float getY() //retorna a coordenada y do ponto
 {
-
+return y;
 }
 
 ponto add(ponto p1)//soma x e y atual com as fornecidas, gerando novo ponto
