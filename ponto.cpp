3fa83cf4350c8d0600c03aab52a2b81862aad91c
wps13 <willanepaiva@hotmail.com>
2017-09-08 20:36:48 -0300

    Primeiras implementações

--- ponto.cpp
+++ ponto.cpp
@@ -1,4 +1,5 @@
 #include "ponto.h"
+#include <cmath>
 
 ponto::ponto()
 {
@@ -8,43 +9,54 @@ ponto::ponto()
 
 void setX(float _x) //define a coordenada x do ponto
 {
-x=_x;
+    x=_x;
 
 }
 
 void setY(float _y)//define a coordenada y do ponto
 {
-y=_y;
+    y=_y;
 }
 
 void setXY(float _x, float _y)  //define x e y do ponto
 {
-x=_x;
-y=_y;
+    x=_x;
+    y=_y;
 }
 
 float getX() //retorna a coordenada x do ponto
 {
-return x;
+    return x;
 }
 
 float getY() //retorna a coordenada y do ponto
 {
-return y;
+    return y;
 }
 
 ponto add(ponto p1)//soma x e y atual com as fornecidas, gerando novo ponto
 {
+    ponto p2;
+    p2.x=x+p1.x;
+    p2.y=y+p1.y;
 
+    return p2;
 }
 
 ponto sub(ponto p1)//subtrai x e y atual com as fornecidas, gerando novo ponto
 {
+    ponto p2;
+    p2.x=x-p1.x;
+    p2.y=y-p1.y;
 
+    return p2;
 }
 
 float norma()//Calcula a distância do ponto para a origem do sistema de coordenadas
 {
+    //n é a norma
+    float n;
+    n=sqrt(pwd(x,2)+pwd(y,2));
 
 }
 
@@ -55,5 +67,5 @@ void translada(float a, float b)//Translada o ponto (x,y) de (+a,+b)
 
 void imprime()
 {
-
+    cout<<"Coordenada X: "<<x<<", coordenada Y: "<<y<<endl;
 }
