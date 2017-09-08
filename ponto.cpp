7d0a39780e254c1b0024fefc1a6ffe9142e1fec3
wps13 <willanepaiva@hotmail.com>
2017-09-08 12:40:15 -0300

    Declaração de métodos

--- ponto.cpp
+++ ponto.cpp
@@ -5,3 +5,54 @@ ponto::ponto()
 
 }
 
+
+void setX(float _x) //define a coordenada x do ponto
+{
+
+
+}
+
+void setY(float _y)//define a coordenada y do ponto
+{
+
+}
+
+void setXY(float _x, float _y)  //define x e y do ponto
+{
+
+}
+
+float getX() //retorna a coordenada x do ponto
+{
+
+}
+
+float getY() //retorna a coordenada y do ponto
+{
+
+}
+
+ponto add(ponto p1)//soma x e y atual com as fornecidas, gerando novo ponto
+{
+
+}
+
+ponto sub(ponto p1)//subtrai x e y atual com as fornecidas, gerando novo ponto
+{
+
+}
+
+float norma()//Calcula a distância do ponto para a origem do sistema de coordenadas
+{
+
+}
+
+void translada(float a, float b)//Translada o ponto (x,y) de (+a,+b)
+{
+
+}
+
+void imprime()
+{
+
+}
--- ponto.h
+++ ponto.h
@@ -6,6 +6,20 @@ class ponto
 {
 public:
     ponto();
+private :
+    float x,y;
 };
+
+void setX(float _x);
+void setY(float _y);
+void setXY(float _x,float _y);
+float getX();
+float getY();
+ponto add(ponto p1);
+ponto sub(ponto p1);
+float norma();
+void translada(float a,float b);
+void imprime();
+
 
 #endif // PONTO_H
